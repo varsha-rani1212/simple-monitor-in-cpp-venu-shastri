cf155508c4ec8fe54d4106cbcfbd5fad2599229c
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-09 12:11:31 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -9,29 +9,26 @@ void printToConsole(string statement)         //impure function
 
 bool checkBattery(float temperature)
 {
-  if(temperature < 0 || temperature > 45) { 
-    printToConsole("Temperature out of range!\n");
-    return false;
-  }
-  return true;
+  if(temperature >= 0 && temperature <= 45)
+    return true;
+ 
+   return false;
 }
 
 bool checkSoc(float soc)
 {
-    if(soc < 20 || soc > 80) {
-    printToConsole("State of Charge out of range!\n");
+    if(soc >= 20 && soc <= 80) 
+      return true;
+
     return false;
-  }
-  return true;
 }
 
 bool checkChargeRate(float chargeRate)
 {
-   if(chargeRate > 0.8) {
-    printToConsole( "Charge Rate out of   range!\n);
-    return false;
-  }
-  return true;
+   if(chargeRate <= 0.8) 
+    return true;
+   
+  return false;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
