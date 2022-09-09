370cc2031b42f5479a04d453601fc0141ff7b4e1
Varsha Rani <98580614+varsha-rani1212@users.noreply.github.com>
2022-09-09 12:07:13 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,18 +2,43 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of   range!\n";
+void printToConsole(string statement)         //impure function 
+{
+  cout << statement;
+}
+
+bool checkBattery(float temperature)
+{
+  if(temperature < 0 || temperature > 45) { 
+    printToConsole("Temperature out of range!\n");
     return false;
   }
   return true;
+}
+
+bool checkSoc(float soc)
+{
+    if(soc < 20 || soc > 80) {
+    printToConsole("State of Charge out of range!\n");
+    return false;
+  }
+  return true;
+}
+
+bool checkChargeRate(float chargeRate)
+{
+   if(chargeRate > 0.8) {
+    printToConsole( "Charge Rate out of   range!\n);
+    return false;
+  }
+  return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  
+   if(checkBattery == true && checkSoc == true && checkChargeRate == true)
+      return true;
+   return false;
 }
 
 int main() {
